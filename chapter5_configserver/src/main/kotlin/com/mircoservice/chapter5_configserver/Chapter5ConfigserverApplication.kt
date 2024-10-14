24f1c5df187a9f4330c78278334e9b960a032a46
Haseop Lee <haservi@naver.com>
2024-10-15 00:06:07 +0900

    init: chapter5 config server 프로젝트 추가

---
+++ chapter5_configserver/src/main/kotlin/com/mircoservice/chapter5_configserver/Chapter5ConfigserverApplication.kt
@@ -0,0 +1,11 @@
+package com.mircoservice.chapter5_configserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter5ConfigserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<Chapter5ConfigserverApplication>(*args)
+}
