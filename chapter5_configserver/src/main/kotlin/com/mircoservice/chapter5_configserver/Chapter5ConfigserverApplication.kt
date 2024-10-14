2e82f9bb3aeee43b8bacc12c228bae7fb412a5bc
Haseop Lee <haservi@naver.com>
2024-10-15 00:58:08 +0900

    feat: 스프링 클라우드 컨피그 설정으로 환경별 프로퍼티 설정

--- chapter5_configserver/src/main/kotlin/com/mircoservice/chapter5_configserver/Chapter5ConfigserverApplication.kt
+++ chapter5_configserver/src/main/kotlin/com/mircoservice/chapter5_configserver/Chapter5ConfigserverApplication.kt
@@ -2,10 +2,12 @@ package com.mircoservice.chapter5_configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
-class Chapter5ConfigserverApplication
+@EnableConfigServer
+class Chapter5ConfigServerApplication
 
 fun main(args: Array<String>) {
-	runApplication<Chapter5ConfigserverApplication>(*args)
+    runApplication<Chapter5ConfigServerApplication>(*args)
 }
